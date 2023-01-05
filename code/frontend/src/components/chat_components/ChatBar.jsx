a5223ee91bd2d8b53f6859908d7782a19eae3241
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-01-05 19:43:38 +0200

    All Message types added

--- code/frontend/src/components/chat_components/ChatBar.jsx
+++ code/frontend/src/components/chat_components/ChatBar.jsx
@@ -1,52 +1,59 @@
-import * as React from 'react';
-import {useState} from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import "./ChatBar.css";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
 const CustomButton = styled(Button)(() => ({
-      fontWeight:"bold",
-      padding: "0 100px",
-"&:hover":{
-  backgroundColor:"none"
-}}));
-//
-// const Item2 = styled(Button)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#6192e5',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//   }));
-//
-//   const Item3 = styled(Button)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//   }));
+  fontWeight: "bold",
+  padding: "0 100px",
+  "&:hover": {
+    backgroundColor: "none",
+  },
+}));
 
 export default function AutoGrid() {
-    const [active , setActive] =useState('main');
+  const [active, setActive] = useState("main");
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={0}>
-        <Grid item1 xs={0.2}>
+        <Grid item1 xs={0.2}></Grid>
+        <Grid xs={3.8}>
+          <CustomButton
+            onClick={() => setActive("a")}
+            sx={{
+              backgroundColor: active === "a" ? "#6192e5" : "#EEEEEE",
+              color: active === "a" ? "#fff" : "black",
+            }}
+          >
+            Party A
+          </CustomButton>
         </Grid>
         <Grid xs={3.8}>
-          <CustomButton onClick={()=>setActive("a")} sx={{backgroundColor: active ==="a" ? "#6192e5" : "#EEEEEE" , color: active==="a"? "#fff" : "black"}} >Party A</CustomButton>
-        </Grid>
-        <Grid  xs={3.8} >
-          <CustomButton onClick={()=>setActive("main")} sx={{backgroundColor: active ==="main" ? "#6192e5" : "#EEEEEE" , color: active==="main"? "#fff" : "black"}}>Main</CustomButton>
+          <CustomButton
+            onClick={() => setActive("main")}
+            sx={{
+              backgroundColor: active === "main" ? "#6192e5" : "#EEEEEE",
+              color: active === "main" ? "#fff" : "black",
+            }}
+          >
+            Main
+          </CustomButton>
         </Grid>
         <Grid xs={3.8}>
-          <CustomButton onClick={()=>setActive("b")} sx={{backgroundColor: active ==="b" ? "#6192e5" : "#EEEEEE" , color: active==="b"? "#fff" : "black"}}>Party B</CustomButton>
+          <CustomButton
+            onClick={() => setActive("b")}
+            sx={{
+              backgroundColor: active === "b" ? "#6192e5" : "#EEEEEE",
+              color: active === "b" ? "#fff" : "black",
+            }}
+          >
+            Party B
+          </CustomButton>
         </Grid>
-        <Grid item1 xs={0.2}>
-        </Grid>
+        <Grid item1 xs={0.2}></Grid>
       </Grid>
     </Box>
   );
--- code/frontend/src/components/chat_components/OppositeMessage.jsx
+++ code/frontend/src/components/chat_components/OppositeMessage.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import "./SelfMessage.css";
+import "./OppositeMessage.css";
 
-const SelfMessage = () => {
+const OppositeMessage = () => {
   return (
     <React.Fragment>
       <Grid container spacing={0} className="container">
         <Grid item sm={3} className="spacer"></Grid>
 
         <Grid item sm={6} className="content">
+          <div className="B-circle">B</div>
+
           <div className="message">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem
             minus eius qui pariatur amet illo temporibus inventore cum. Soluta,
             reiciendis!
           </div>
-          <div className="A-circle">A</div>
         </Grid>
 
         <Grid item sm={3} xs={8} className="spacer"></Grid>
@@ -23,4 +24,4 @@ const SelfMessage = () => {
   );
 };
 
-export default SelfMessage;
+export default OppositeMessage;
