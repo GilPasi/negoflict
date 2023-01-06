189142e0a273540b218d2354e80356d4c555223b
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-01-06 12:38:24 +0200

    Add CreateUserOptionPage

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./components/Login.css";
-
 import Chat from "./components/chat_components/Chat";
 import Login from "./components/Login";
 import Paper from "@mui/material/Paper";
@@ -15,6 +14,7 @@ function App() {
         <Chat></Chat>
         <CreateUserOptionPage></CreateUserOptionPage>
       </Paper>
+      
     </React.Fragment>
   );
 }
