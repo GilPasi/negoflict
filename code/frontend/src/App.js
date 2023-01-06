aed03c6c4c1ea90a7aa8f9746d7e3c4849e115de
אביב אסולין <avivasulin1211@gmail.com>
2023-01-06 11:23:38 +0200

    finish misson

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,31 +1,21 @@
+import React from "react";
+import "./components/Login.css";
 
-import React from 'react';
-import './components/Login.css';
-
- import Chat from "./components/chat_components/Chat"
-import Login from "./components/Login"
-import Paper from '@mui/material/Paper';
-
-
-
-
+import Chat from "./components/chat_components/Chat";
+import Login from "./components/Login";
+import Paper from "@mui/material/Paper";
+import NewUser from "./components/NewUser";
 
 function App() {
   return (
     <React.Fragment>
       <Login></Login>
       <Paper>
-      <Chat></Chat>
+        <Chat></Chat>
       </Paper>
+      <NewUser />
     </React.Fragment>
-
-
-      
-
   );
 }
 
 export default App;
-
-
-
