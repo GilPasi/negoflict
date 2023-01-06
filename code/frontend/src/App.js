5c8ec0e133345f08ba54d01ae7a87fe255780a64
Yinonlevy94 <91956854+Yinonlevy94@users.noreply.github.com>
2023-01-06 11:17:08 +0200

    menu bar now is a button and has options to select

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -3,15 +3,17 @@ import './components/Login.css';
 import Chat from "./components/chat_components/Chat"
 import Login from "./components/Login"
 import Paper from '@mui/material/Paper';
+import NavBar from './components/chat_components/NavBar';
+
 function App() {
   return (
-
-    <React.Fragment>
-      <Login></Login>
-      <Paper>
-      <Chat></Chat>
-      </Paper>
-    </React.Fragment>
+    <NavBar></NavBar>
+    // <React.Fragment>
+    //   <Login></Login>
+    //   <Paper>
+    //   <Chat></Chat>
+    //   </Paper>
+    // </React.Fragment>
 
   );
 }
