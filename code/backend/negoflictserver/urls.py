8592ba568741f2ad62ad22a90714f01516a9abb3
Henberti <hennnb@outlook.co.il>
2023-01-09 04:17:35 +0200

    fix

--- code/backend/negoflictserver/urls.py
+++ code/backend/negoflictserver/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('',include('negoflict_app.urls')),
+    path('users/',include('negoflict_app.urls')),
+    path('session/',include('session.urls')),
+    
 ]
