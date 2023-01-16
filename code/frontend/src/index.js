7a2996bca50d9995addc08356a16c2683cbc6862
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-01-16 16:35:41 +0200

    prepare to weekly demo

--- code/frontend/src/index.js
+++ code/frontend/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NewUser from './components/NewUser';
+import CreateUserOptionPage from './components/CreateUserOptionPage';
+import Chat from './components/chat_components/Chat';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Chat/>
+    <CreateUserOptionPage/>
+    <NewUser></NewUser>
   </React.StrictMode>
 );
 
