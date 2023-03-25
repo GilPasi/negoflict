f9c296964e9c967c10ddf818571eccb064a38029
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-03-25 22:35:14 +0300

    uncomment app comp

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -22,27 +22,29 @@ import useNodeS from './hooks/useNodeS.js';
   
 
   return (
-    <div className="app">
-
-      <ChatPage/>
-      {/* <LoginPage/>
-      <CaseFormPage/>
-      <CasePage/> */}
-
-
-
-
-
-
-
-     
-
-   
-
+    <div className="app">      
+      <Routes>
+            
+      <Route path='/' element={<Layout/>}>
+        {/* public routes */}
       
-
-
-          
+      
+      <Route  path='/login' element={<LoginPage/>}/>
+      
+      {/* protected routes */}
+      {isActive?(
+      
+      <Route  path='/' element={<RequireAuth/>}>
+        <Route path='user/*' element={<UserLandingPage/>}/>
+        <Route path='mediator/*' element={<MediatorLandingPage/>}/>
+        <Route path='admin/*' element={<SuperUserLandingPage/>}/>
+        <Route path='chat' element={<ChatPage/>}/>
+      </Route>
+      ):(<div><h1>not active</h1></div>)}
+        {/* catch */}
+      
+      </Route>
+      </Routes>          
     </div>
   );
 }
@@ -50,26 +52,3 @@ import useNodeS from './hooks/useNodeS.js';
 export default App;
 
 
-
-// <Routes>
-      
-// <Route path='/' element={<Layout/>}>
-//   {/* public routes */}
- 
-
-// <Route  path='/login' element={<LoginPage/>}/>
-
-// {/* protected routes */}
-// {isActive?(
-
-// <Route  path='/' element={<RequireAuth/>}>
-//   <Route path='user/*' element={<UserLandingPage/>}/>
-//   <Route path='mediator/*' element={<MediatorLandingPage/>}/>
-//   <Route path='admin/*' element={<SuperUserLandingPage/>}/>
-//   <Route path='chat' element={<ChatPage/>}/>
-// </Route>
-// ):(<div><h1>not active</h1></div>)}
-//   {/* catch */}
-
-// </Route>
-// </Routes>
