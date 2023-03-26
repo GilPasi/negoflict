086b4888b9b305c9fb4f06ba7152869554c2cbc4
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-03-26 22:54:33 +0300

    conf res

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -8,7 +8,7 @@ import RequireAuth from './components/Authentication/RequireAuth'
 import Layout from './components/general/Layout';
 import {UserLandingPage,MediatorLandingPage,SuperUserLandingPage} from './pages/LandingPage';
 import './App.css'
-import useNodeS from './hooks/useNodeS.js';
+
 
 
 
@@ -54,26 +54,3 @@ import useNodeS from './hooks/useNodeS.js';
 export default App;
 
 
-
-      // <Routes>
-            
-      // <Route path='/' element={<Layout/>}>
-      //   {/* public routes */}
-      
-      
-      // <Route  path='/login' element={<LoginPage/>}/>
-      
-      // {/* protected routes */}
-      // {isActive?(
-      
-      // <Route  path='/' element={<RequireAuth/>}>
-      //   <Route path='user/*' element={<UserLandingPage/>}/>
-      //   <Route path='mediator/*' element={<MediatorLandingPage/>}/>
-      //   <Route path='admin/*' element={<SuperUserLandingPage/>}/>
-      //   <Route path='chat' element={<ChatPage/>}/>
-      // </Route>
-      // ):(<div><h1>not active</h1></div>)}
-      //   {/* catch */}
-      
-      // </Route>
-      // </Routes>          
