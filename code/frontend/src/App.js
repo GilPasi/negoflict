459606de2a11149c608d11ed7a3d69668372bb5d
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-04-05 22:09:37 +0300

    uncomment app

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -25,28 +25,27 @@ import SystemOptionsPage from './pages/SystemOptionsPage.js';
   
 
   return (
-      // <Routes>
-      //     <Route path='/' element={<Layout/>}>
-      //       {/* public routes */}
-
-      //     <Route  path='/login' element={<LoginPage/>}/>
-          
-      //     {/* protected routes */}
-      //     {isActive?(
-
-      //     <Route  path='/' element={<RequireAuth/>}>
-      //       <Route path='user/*' element={<UserLandingPage/>}/>
-      //       <Route path='mediator/*' element={<MediatorLandingPage/>}/>
-      //       <Route path='admin/*' element={<SuperUserLandingPage/>}/>
-      //       {/* <Route path='chat' element={<ChatPage/>}/> */}
-      //     </Route>
-      //     ):(<div><h1>not active</h1></div>)}
-      //       {/* catch */}
-          
-      //     </Route>
-      //   </Routes>
     <div className="app">  
-      <SystemOptionsPage/>
+      <Routes>
+          <Route path='/' element={<Layout/>}>
+            {/* public routes */}
+
+          <Route  path='/login' element={<LoginPage/>}/>
+          
+          {/* protected routes */}
+          {isActive?(
+
+          <Route  path='/' element={<RequireAuth/>}>
+            <Route path='user/*' element={<UserLandingPage/>}/>
+            <Route path='mediator/*' element={<MediatorLandingPage/>}/>
+            <Route path='admin/*' element={<SuperUserLandingPage/>}/>
+            {/* <Route path='chat' element={<ChatPage/>}/> */}
+          </Route>
+          ):(<div><h1>not active</h1></div>)}
+            {/* catch */}
+          
+          </Route>
+        </Routes>
 
 
     </div>
