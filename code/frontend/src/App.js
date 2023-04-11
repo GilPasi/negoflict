a370d42aa6160f1002f76c9dcf4b39380d815f12
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-04-12 00:06:56 +0300

    conf

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -9,12 +9,8 @@ import RequireAuth from './components/Authentication/RequireAuth'
 import Layout from './components/general/Layout.js';
 import {UserLandingPage,MediatorLandingPage,SuperUserLandingPage} from './pages/LandingPage';
 import './App.css'
-import Chat from 'agora-chat-uikit/lib/EaseChat/chat/index.js';
 
-import LoadingBar from "./components/general/LoadingBar.js"
-import Modal from "./components/general/Modal.js"
-import ToolBar from './components/general/ToolBar.js';
-
+import AddMediatorPage from './pages/AddMediatorPage.js';
 
 
 
@@ -22,31 +18,31 @@ import ToolBar from './components/general/ToolBar.js';
  const App=()=> {
   const isActive = useInactivityRedirect();
   
-    // <Routes>
-    //     <Route path='/' element={<Layout/>}>
-    //       {/* public routes */}
 
-    //     <Route  path='/login' element={<LoginPage/>}/>
-        
-    //     {/* protected routes */}
-    //     {isActive?(
-
-    //     <Route  path='/' element={<RequireAuth/>}>
-    //       <Route path='user/*' element={<UserLandingPage/>}/>
-    //       <Route path='mediator/*' element={<MediatorLandingPage/>}/>
-    //       <Route path='admin/*' element={<SuperUserLandingPage/>}/>
-    //       {/* <Route path='chat' element={<ChatPage/>}/> */}
-    //     </Route>
-    //     ):(<div><h1>not active</h1></div>)}
-    //       {/* catch */}
-        
-    //     </Route>
-    // </Routes>
     const a =<div>hellow world</div>      
   return(
     <div>
-      <ChatPage isMediator={true} 
-      />
+          <Routes>
+        <Route path='/' element={<Layout/>}>
+          {/* public routes */}
+
+        <Route  path='/login' element={<LoginPage/>}/>
+        
+        {/* protected routes */}
+        {isActive?(
+
+        <Route  path='/' element={<RequireAuth/>}>
+          <Route path='user/*' element={<UserLandingPage/>}/>
+          <Route path='mediator/*' element={<MediatorLandingPage/>}/>
+          <Route path='admin/*' element={<SuperUserLandingPage/>}/>
+          <Route path='chat' element={<ChatPage/>}/>
+        </Route>
+        ):(<div><h1>not active</h1></div>)}
+          {/* catch */}
+        
+        </Route>
+    </Routes>
+
     </div>
 
   )
