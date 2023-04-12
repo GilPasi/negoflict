e0756b4e738cd76b96c83ddb69a494efcd64fb79
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-04-12 22:22:40 +0300

    res conf

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -14,17 +14,10 @@ import ChatView from './components/chat/ChatView';
   
 
   return(
-<<<<<<< HEAD
-    <div>
-      <Routes>
-          <Route path='/' element={<Layout/>}>
-            {/* public routes */}
-=======
     <div className="app">
       <Routes>
         <Route path='/' element={<Layout/>}>
           {/* public routes */}
->>>>>>> ae474cd61ba0cdd88f321a77fb95a10e87318faa
 
             <Route  path='/login' element={<LoginPage/>}/>
             
@@ -39,26 +32,10 @@ import ChatView from './components/chat/ChatView';
           ):(<div><h1>not active</h1></div>)}
             {/* catch */}
           
-<<<<<<< HEAD
           </Route>
-      </Routes>      
-=======
-          {/* protected routes */}
-          {isActive?(
+      </Routes>    
+    </div>  
 
-        <Route  path='/' element={<RequireAuth/>}>
-          <Route path='user/*' element={<UserLandingPage/>}/>
-          <Route path='mediator/*' element={<MediatorLandingPage/>}/>
-          <Route path='admin/*' element={<SuperUserLandingPage/>}/>
-        </Route>
-        ):(<div><h1>not active</h1></div>)}
-          {/* catch */}
-        
-        </Route>
-      </Routes>
->>>>>>> ae474cd61ba0cdd88f321a77fb95a10e87318faa
-    </div>
-  )
-}
+)}
 export default App;
 
