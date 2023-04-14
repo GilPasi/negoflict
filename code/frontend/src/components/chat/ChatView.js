c998dfb3108f91ea9dde799f852be0ad6d042d75
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-04-14 17:33:25 +0300

    chat size is now responsive

--- code/frontend/src/components/chat/ChatView.js
+++ code/frontend/src/components/chat/ChatView.js
@@ -4,9 +4,33 @@ import ShuttleSwitch from '../general/ShuttleSwitch'
 import MessageList from './MessageList'
 import UserPanel from '../general/UserPanel'
 import ToolBar from '../general/ToolBar'
+import { useState, useEffect , useRef} from 'react';
 
 
-const ChatView = ({isMediator, caseId,activeGroup,handleSend, handleShuttle, isShuttled})=>{
+
+const ChatView = ({isMediator, caseId,activeGroup,handleSend, handleShuttle, isShuttled, fetch})=>{
+
+
+    const [size, setSize] = useState(window.innerHeight);
+
+
+
+    useEffect(() => {
+ 
+
+      function handleResize() {
+        setSize(window.innerHeight);
+      }
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
+    
+
 
     const setInputHeight =(element, defaultHeight)=>{
         if(element){
@@ -18,90 +42,63 @@ const ChatView = ({isMediator, caseId,activeGroup,handleSend, handleShuttle, isS
     }
     const setInputValue = ()=>{
         const msg = document.querySelector("#cp--input-tb").value;
-    
 
         if(!msg)return
-            handleSend(msg)
+
+        handleSend(msg)
+
+        document.querySelector("#cp--input-tb").value='';//Eventually clean the text box
+
     }
+    
+
+
+
     return(
-        <article className="cp" >
-
-            
-            <div
-                className="cp--shuttle-block"
-                style={{
-                    display:isShuttled? "": "none", //deleted &&(msgScreen===texts)
-                    backgroundColor: "#011202db",
-                    opacity:"0.95"
-                }}
+        <article 
+            className="cp" 
+            style={{ 
+                height: `${size}px`,
+                display: 'grid',
+                gridTemplateRows:'240px 1fr 192px ',
                 
-                >
-                    <div className="cp--shuttle-block-msg">
-                        {/* Render the right message according the user */}
-                            {isMediator?(
-                                <p>
-                                    Ths is is a Shuttle mode.
-                                    <br/>
-                                    You cannot contact the second party.
-                                    <br/>
-                                    <br/>
-                                    You can contact Mediator in your private chat.
-                                </p>
-                                ):(
-                                  <p>
-                                    You turned on the shuttle mode.<br/>
-                                    Only private chats are available for all users.
-                                  </p> 
-                                  )}
-                    </div>
-
-                </div>
-
-                <div className="cp--shuttle-block"></div>
-                <div
-                 style={{
-                    position:"fixed",
-                    top:"0",
-                    left:'0',
-                    right:'0',
-                    backgroundColor:"white", //This is crucial for hiding the MessageList
-                    zIndex:"1",
-                }}>
-
+            }}
+            
+        >
+                <header>
                     <Header isLarge={false}/>
                     <ToolBar conflictName="A political conflict" id={caseId}/>
                     <ShuttleSwitch isMediator={isMediator}/>
-                </div>
-                <div >
-                <MessageList activeGroup={activeGroup}/>
-            </div>
+                </header>
 
-            <div>
-            <footer className="cp--footer">
-                <div className="cp--input">
-                    <span className="material-symbols-outlined cp--help">
-                        help
-                    </span>
-                    <textarea
-                        dir="auto"
-                        onChange={event=>setInputHeight(event, '10px')}
-                        className="cp--input-box"
-                        id="cp--input-tb"
-                     />
-
-                        <button className="cp--input-btn" onClick={setInputValue}>
-                            <span className="material-symbols-outlined cp--send" >
-                                send
-                            </span>
-                        </button>
+                <div>
+                    <MessageList activeGroup={activeGroup} maxHeight={`${size-432}px`}/>
                 </div>
-                <UserPanel
-                    handleSwitch={handleShuttle}
-                    isSwitched={isShuttled}
-                    isComplex={true}
-                />
-            </footer>
-            </div>
+
+                <footer className="cp--footer">
+                    <div className="cp--input">
+                        <span className="material-symbols-outlined cp--help">
+                            help
+                        </span>
+                        <textarea
+                            dir="auto"
+                            onChange={event=>setInputHeight(event, '10px')}
+                            className="cp--input-box"
+                            id="cp--input-tb"
+                        />
+
+                            <button className="cp--input-btn" onClick={setInputValue}>
+                                <span className="material-symbols-outlined cp--send" >
+                                    send
+                                </span>
+                            </button>
+                    </div>
+                    <UserPanel
+                        handleSwitch={handleShuttle}
+                        isSwitched={isShuttled}
+                        isComplex={true}
+                    />
+                </footer>
         </article>
      
     )
@@ -112,4 +109,34 @@ export default ChatView
 
 
 
+// <div
+//                 className="cp--shuttle-block"
+//                 style={{
+//                     display:isShuttled? "": "none", //deleted &&(msgScreen===texts)
+//                     backgroundColor: "#011202db",
+//                     opacity:"0.95"
+//                 }}
+                
+//                 >
+//                     <div className="cp--shuttle-block-msg">
+//                         {/* Render the right message according the user */}
+//                             {isMediator?(
+//                                 <p>
+//                                     Ths is is a Shuttle mode.
+//                                     <br/>
+//                                     You cannot contact the second party.
+//                                     <br/>
+//                                     <br/>
+//                                     You can contact Mediator in your private chat.
+//                                 </p>
+//                                 ):(
+//                                 <p>
+//                                     You turned on the shuttle mode.<br/>
+//                                     Only private chats are available for all users.
+//                                 </p> 
+//                                 )}
+//                     </div>
 
+//                 </div>
+
+//                 <div className="cp--shuttle-block"></div>
