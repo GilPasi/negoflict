d9357d9e602d4f294bb6b5cb666bc8d9436953b5
henberti <hennnb@outlook.co.il>
2023-04-24 03:19:19 +0300

    env fix

--- code/frontend/src/WebIM.js
+++ code/frontend/src/WebIM.js
@@ -1,7 +1,8 @@
 import {EaseChat} from 'agora-chat-uikit'
 import {AppKey} from './utils/agoraCradential'
 
-const WebIM = EaseChat.getSdk({appkey:'71864841#1052059'})
+
+const WebIM = EaseChat.getSdk({appkey:AppKey})
 
 
 export default WebIM
