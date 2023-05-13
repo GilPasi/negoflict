abd73d4d0ad461bd5894b1631e5ba74aa34eaea4
henberti <hennnb@outlook.co.il>
2023-05-13 12:32:48 +0300

    finish adding one user

--- code/frontend/src/pages/CreateSelfUserPage.js
+++ code/frontend/src/pages/CreateSelfUserPage.js
@@ -3,11 +3,17 @@ import UserForm from "../components/general/userForm"
 import Header from "../components/general/Header"
 import { useEffect, useState } from "react"
 import Button from "../components/general/Button"
-import { useRegisterOneUserMutation, } from "../store"
+import { useRegisterOneUserMutation,useCreateContactMutation,useCreateUsersMutation,useLazyIsEmailExistQuery } from "../store"
+import { useSelector } from "react-redux"
 
-const CreateSelfUser = ()=>{
+const CreateSelfUser = ({fulfiled})=>{
     const [formData,setFormData] = useState({})
     const [valid,setValid] = useState(true)
+    const [registerUser] = useRegisterOneUserMutation()
+    const [createContact] = useCreateContactMutation()
+    const [createUser] = useCreateUsersMutation()
+    const [isEmailValid] = useLazyIsEmailExistQuery()
+    const {id} = useSelector(state=>state.user)
     
     useEffect(()=>{
         if(Object.keys(formData).length >=4)
@@ -20,9 +26,34 @@ const CreateSelfUser = ()=>{
         setFormData(prevState=>({...prevState,[name]:value}))
     }
 
-    const handleSubmit = (event)=>{
+    const handleSubmit =async (event)=>{
         event.preventDefault()
-        console.log(formData)
+        const {phoneNumber,email,first_name, last_name} = formData
+        const modEmail = email.replace(/[^\w\s]/gi, '')
+        const modPass = `Negoflict${phoneNumber}`
+
+        let { data, error } = await isEmailValid({ email:email });
+          
+          if (data === true || error) {
+              console.log(`this email ${email} is already exist \n`)
+              return
+          }
+
+        registerUser({username:modEmail,password:modPass,first_name:first_name})
+        .catch(err=>console.log(err))
+        createUser({users:[{username:email,password:modPass,first_name:first_name,last_name:last_name, email:email}]}).then(res=>{
+            console.log('medi: ',id)
+            console.log('user ',res.data[0].id)
+            console.log(res)
+            createContact({mediator_id:id,user_id:res.data[0].id})
+            .then(()=>fulfiled('success'))
+            .catch(err=>{
+                console.log(err)
+                fulfiled('error')
+
+            })
+        })
+        .catch(err=>console.log(err))
     }
 
 
@@ -36,7 +67,7 @@ const CreateSelfUser = ()=>{
                  required={true}
                  />
 
-                 <Button text={'Submit'} size={'medium'} disabled={valid}/>
+                 <Button onClick={handleSubmit} text={'Submit'} size={'medium'} disabled={valid}/>
             </form>
         </div>
     )
