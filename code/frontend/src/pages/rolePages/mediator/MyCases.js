62f4443fed9ba7fb1c35b701f39f7dc95afa418e
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-05-27 19:12:05 +0300

    animation added

--- code/frontend/src/pages/rolePages/mediator/MyCases.js
+++ code/frontend/src/pages/rolePages/mediator/MyCases.js
@@ -15,28 +15,27 @@ const MyCases = ({isMediator, open_close})=>{
     if(isLoading)return <Loader/>
     if(error)return alert('Eror refresh the page please')
     return(
-        <article className="page">
+        <article style={{ minWidth: "var(--screen-minw)",minHeight: "var(--screen-minh)",}}>
             <Header isLarge={true}/>
 
-            
             <h1 className='title-large'> My cases </h1>
-            <div style={{height: '45vh',overflowY: 'scroll',}} >
+            <div style={{height: '50vh',overflowY: 'scroll',}} >
                 {isSuccess &&
                     cases.map(caseData=>(
                         <div key={caseData.id}>
-                        {/* <CaseReview 
+                        <CaseReview 
                             caseName="Orgainzation - work spy"
                             caseId="2e55e4"
                             creationDate="April 2023"
                             caseIndex="1"
-                        /> */}
+                        />
                             
-                        <TextHolder 
+                        {/* <TextHolder 
                             addOns={caseData.category}
                             caseData={caseData} 
                             withInfo={true}
                             hasExit={false}
-                        />
+                        /> */}
                         </div>
                 ))}
             </div>
